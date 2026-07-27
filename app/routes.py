64a89003f17903ec34f610a681366d8c802e9cff
minkhantkyaw <minkhantkyaw@my.jcu.edu.au>
2026-07-27 12:36:32 +0630

    Change price to BigIntegger and reset database

--- app/routes.py
+++ app/routes.py
@@ -202,3 +202,10 @@ def logout():
         url_for("main.login")
     )
 
+@main.route("/reset-db")
+def reset_db():
+
+    db.drop_all()
+    db.create_all()
+
+    return "Database reset successfully"
